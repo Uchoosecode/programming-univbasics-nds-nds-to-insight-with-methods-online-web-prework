4e7861949c9da09966ddbb2429a04e5a56e176be
Ayako Wiltshire <awittyl44@gmail.com>
2019-11-18 13:44:39 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -18,7 +18,7 @@ directors_name = nds[row_index][:name]
   # The directors_name is a variable of all the directors in the nds.
   
 result[directors_name] = 0 
-# This takes the variable directors_name, adds it into the gross hash and gives it a value of 0 as a counter.
+# This takes the variable directors_name, adds it into the result hash and gives it a value of 0 as a counter.
 
 movies = nds[row_index][:movies]
   # The movies variable show the names of all the movies.
@@ -41,14 +41,25 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  director_data = directors_database 
-  row = 0 
-  movies = directors_database[row][:movies]
-  grosses = []
-  while row < movies.length do 
-    movies[row]
-    row += 1
-    grosses <<  += [movies[row][:worldwide_gross]]
-    p grosses
-  end
+  row_index = 0 
+  while row_index < director_data[:movies].length do 
+   grosses = director_data[:movies][row_index][:worldwide_gross]
+    
+  grand_gross = 0 
+p grosses
+    element_index = 0 
+  while element_index < grosses.length do
+    
+    grand_gross += grosses[element_index]
+    
+    
+  
+  element_index += 1 
+
 end
+p grand_gross
+row_index += 1 
+end
+
+end
+
